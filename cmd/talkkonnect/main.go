f4beb7bab8d0686d160f09bd464d373bb755c263
Garrett Anderson <gband85@mailfence.com>
2024-04-07 18:47:14 -0500

    update main.go

--- cmd/talkkonnect/main.go
+++ cmd/talkkonnect/main.go
@@ -28,67 +28,68 @@
  *
  */
 
-package main
+ package main
 
-import (
-	"flag"
-	"github.com/gband85/talkkonnect"
-	"fmt"
-	"log"
-	"os"
-	"runtime/pprof"
-	"runtime"
-)
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
-var serverindex = flag.String("serverindex", "0", "jump to server index [n]")
-
-
-func main() {
-
-	config := flag.String("config", "/usr/local/etc/talkkonnect.xml", "full path to talkkonnect.xml configuration file")
-
-	flag.Usage = talkkonnectusage
-	flag.Parse()
-
-  if *cpuprofile != "" {
-        f, err := os.Create(*cpuprofile)
-        if err != nil {
-            log.Fatal("could not create CPU profile: ", err)
-        }
-        if err := pprof.StartCPUProfile(f); err != nil {
-            log.Fatal("could not start CPU profile: ", err)
-        }
-        defer pprof.StopCPUProfile()
-    }
-
-    if *memprofile != "" {
-        f, err := os.Create(*memprofile)
-        if err != nil {
-            log.Fatal("could not create memory profile: ", err)
-        }
-        runtime.GC() // get up-to-date statistics
-        if err := pprof.WriteHeapProfile(f); err != nil {
-            log.Fatal("could not write memory profile: ", err)
-        }
-        f.Close()
-    }
-
-	talkkonnect.Init(*config, *serverindex)
-
-
-
-}
-
-func talkkonnectusage() {
-	fmt.Println("---------------------------------------------------------------------------------------")
-	fmt.Println("Usage: talkkonnect [-config=[full path and file to talkkonnect.xml configuration file]]")
-	fmt.Println("By Suvir Kumar <suvir@talkkonnect.com>")
-	fmt.Println("For more information visit http://www.talkkonnect.com and github.com/talkkonnect")
-	fmt.Println("---------------------------------------------------------------------------------------")
-	fmt.Println("-config=/home/talkkonnect/gocode/src/github.com/talkkonnect/talkkonnect/talkkonnect.xml")
-	fmt.Println("-serverindex=[n] for the index of the enabled server to connect to in XML file")
-	fmt.Println("-version for the version")
-	fmt.Println("-help for this screen")
-}
+ import (
+     "flag"
+     "github.com/gband85/talkkonnect"
+     "fmt"
+     "log"
+     "os"
+     "runtime/pprof"
+     "runtime"
+ )
+ 
+ var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
+ var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+ var serverindex = flag.String("serverindex", "0", "jump to server index [n]")
+ 
+ 
+ func main() {
+ 
+     config := flag.String("config", "/home/talkkonnect/gocode/src/github.com/talkkonnect/talkkonnect/talkkonnect.xml", "full path to talkkonnect.xml configuration file")
+ 
+     flag.Usage = talkkonnectusage
+     flag.Parse()
+ 
+   if *cpuprofile != "" {
+         f, err := os.Create(*cpuprofile)
+         if err != nil {
+             log.Fatal("could not create CPU profile: ", err)
+         }
+         if err := pprof.StartCPUProfile(f); err != nil {
+             log.Fatal("could not start CPU profile: ", err)
+         }
+         defer pprof.StopCPUProfile()
+     }
+ 
+     if *memprofile != "" {
+         f, err := os.Create(*memprofile)
+         if err != nil {
+             log.Fatal("could not create memory profile: ", err)
+         }
+         runtime.GC() // get up-to-date statistics
+         if err := pprof.WriteHeapProfile(f); err != nil {
+             log.Fatal("could not write memory profile: ", err)
+         }
+         f.Close()
+     }
+ 
+     talkkonnect.Init(*config, *serverindex)
+ 
+ 
+ 
+ }
+ 
+ func talkkonnectusage() {
+     fmt.Println("---------------------------------------------------------------------------------------")
+     fmt.Println("Usage: talkkonnect [-config=[full path and file to talkkonnect.xml configuration file]]")
+     fmt.Println("By Suvir Kumar <suvir@talkkonnect.com>")
+     fmt.Println("For more information visit http://www.talkkonnect.com and github.com/talkkonnect")
+     fmt.Println("---------------------------------------------------------------------------------------")
+     fmt.Println("-config=/home/talkkonnect/gocode/src/github.com/talkkonnect/talkkonnect/talkkonnect.xml")
+     fmt.Println("-serverindex=[n] for the index of the enabled server to connect to in XML file")
+     fmt.Println("-version for the version")
+     fmt.Println("-help for this screen")
+ }
+ 
